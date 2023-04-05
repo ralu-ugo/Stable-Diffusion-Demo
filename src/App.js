d4bafa66f330590a23244e4d53164d41eeb39818
Ralu Ugochukwu <ralu2k3@gmail.com>
2023-04-05 14:56:21 +0100

    light/dark mode feature

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 // import rocks from './images/creps.png'
 import './App.css';
 import React from 'react';
+import { createContext, useState } from 'react';
+// import { useEffect } from 'react';
 
+export const ThemeContext = createContext(null)
 function App() {
   const [inputValue, setinputValue] = React.useState("");
   const [imageUrl, setImageUrl] = React.useState();
   const [loading, setloading] = React.useState(false);
+  const [theme, setTheme] = useState('light');
   const engineId = "stable-diffusion-v1-5";
   const apiHost = "https://api.stability.ai";
   const url = `${apiHost}/v1alpha/generation/${engineId}/text-to-image`;
   const apiKey = "sk-MnNX05YvL3g65Fa9K2YLMmhlfALIVORvIncs5nxYhoQyaSHs";
+  console.log(theme);
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "dark" ? "light" : "dark"))
+  }
+  // useEffect(() => {
+  //   document.body.classList.remove('darkmode')
+    // document.body.classList.add('lightmode', theme === 'light');
+  // }, [theme]);
   if (!apiKey) throw new Error("Missing Stability API key.");
   const getResponse = async () => {
     setloading(true);
@@ -42,51 +55,53 @@ function App() {
         });
       }
   return (
-    <body className="App landing">
-      <div class="top">
-        <div>
-          <h1 class ="title">
-            Stable Diffusion
-          </h1>
+    <ThemeContext.Provider value={{theme, toggleTheme}}  >
+      <div className={theme}>
+        <div className="top">
+            <h1 className ="title">Stable Diffusion</h1>
+            <button style={{marginLeft: "24px"}} onClick={toggleTheme}></button>
         </div>
-      </div>
-      <div class="body">
-        <div>
-          <div className='input-area'>
-              <input
-                type="text"
-                onChange={(e) => setinputValue(e.target.value)}
-                value={inputValue}
-                />
-              <submit className="button" onClick={getResponse}>
-                {loading ? "Loading..." : "Generate"}
-              </submit>
+        <div className="body">
+          <div>
+            <div className='input-area'>
+                <input
+                  className={theme}
+                  type="text"
+                  onChange={(e) => setinputValue(e.target.value)}
+                  value={inputValue}
+                  />
+                <button className={theme} onClick={getResponse}>
+                  {loading ? "Loading..." : "Generate"}
+                </button>
+            </div>
+            <p className='desc'>Type in a full descriptive sentence, as if you were writing a caption for a photo. Include as much detail as you see fit, including colors, styles, and emotions. Then click Generate to get your image. </p>
           </div>
-          <p className='desc'>Type in a full descriptive sentence, as if you were writing a caption for a photo. Include as much detail as you see fit, including colors, styles, and emotions. Then click Generate to get your image. </p>
+
+          <div className='img'>
+            {imageUrl ? (
+              <img
+                src={`data:image/png;base64,
+              ${imageUrl.artifacts[0].base64}`}
+                alt={inputValue}
+                style={{ width: "512px", height: "512px", boxShadow: "3px 3px 6px black" }}
+              />) : (
+                loading ? (
+                  <img
+                src={require("./images/rainbow.gif")}
+                alt={"Loading"}
+                style={{ width: "512px", height: "512px" }}
+              />
+                ) : ""
+            )}
+          </div>
         </div>
 
-        <div className='img'>
-          {imageUrl ? (
-            <img
-              src={`data:image/png;base64,
-            ${imageUrl.artifacts[0].base64}`}
-              alt={inputValue}
-              style={{ width: "512px", height: "512px", boxShadow: "3px 3px 6px black" }}
-            />) : (
-              loading ? (
-                <img
-              src={require("./images/rainbow.gif")}
-              alt={"Loading"}
-              style={{ width: "512px", height: "512px" }}
-            />
-              ) : ""
-          )}
-        </div>
+
+        {/* <div>Image Container</div> */}
+
       </div>
 
-      {/* <div>Image Container</div> */}
-
-    </body>
+    </ThemeContext.Provider>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React , { createContext, useState}from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// export const ThemeContext = createContext(null)
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      {/* <ThemeContext.Provider value={{theme, toggleTheme}}  > */}
+        <App />
+      {/* </ThemeContext.Provider> */}
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
